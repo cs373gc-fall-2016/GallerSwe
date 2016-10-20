107f1ca6bfb62ee61ba82ea2b11fea784793fe10
courtbohrer <cbohrer@utexas.edu>
2016-10-20 14:24:07 -0500

    transitions between pages, alpha layout for model pages

--- app.py
+++ app.py
@@ -10,11 +10,23 @@ def root():
 
 @app.route('/artists')
 def artists():
-    return app.send_static_file('andywarhol.html')
+    return app.send_static_file('artists.html')
 
 @app.route('/about')
 def about():
     return app.send_static_file('about.html')
+
+@app.route('/artwork')
+def artwork():
+    return app.send_static_file('artwork.html')
+
+@app.route('/style')
+def style():
+    return app.send_static_file('style.html')
+
+@app.route('/geo')
+def geo():
+    return app.send_static_file('geo.html')
 
 if __name__ == "__main__":
     app.run()
