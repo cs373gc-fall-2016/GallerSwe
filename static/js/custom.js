d0401722787158380d3af4494e1a4d4a968fd48e
courtbohrer <cbohrer@utexas.edu>
2016-10-20 21:26:49 -0500

    took away feature where table row changes color on click

--- static/js/custom.js
+++
@@ -1,7 +0,0 @@
-
-$("#data tr").click(function() {
-    var selected = $(this).hasClass("highlight");
-    $("#data tr").removeClass("highlight");
-    if(!selected)
-            $(this).addClass("highlight");
-});
