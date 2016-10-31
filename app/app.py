dfa7c576ccabe37b663809c721d3d747f759468d
Cullen Bounds <cwillbounds@gmail.com>
2016-10-31 14:30:36 -0500

    templates started

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-    return app.send_static_file('splash.html')
+    return render_template('splash.html')
 
 @app.route('/hello')
 def hello():
