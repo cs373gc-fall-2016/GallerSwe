665235f02399a307807a3ae356f152549d92ca59
Cullen Bounds <cwillbounds@gmail.com>
2016-10-31 13:55:06 -0500

    little change

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import os
-import model
+#import model
 
 from flask import Flask, send_from_directory, render_template
 
@@ -9,16 +9,20 @@ app = Flask(__name__, static_url_path='')
 def root():
     return app.send_static_file('splash.html')
 
+@app.route('/hello')
+def hello():
+    return app.send_static_file('hello.html')
+
 @app.route('/artists')
 def artists():
     return app.send_static_file('artists.html')
 
-@app.route('/artists/<int: id>')
-def artist(id) :
-    data = models.Artist.query.get(id)
-    if data is None :
-        raise Exception
-    return render_template("artist.html", result=data)
+# @app.route('/artists/<int: id>')
+# def artist(id) :
+#     data = models.Artist.query.get(id)
+#     if data is None :
+#         raise Exception
+#     return render_template("artist.html", result=data)
 
 @app.route('/about')
 def about():
@@ -28,34 +32,34 @@ def about():
 def artworks():
     return app.send_static_file('artwork.html')
 
-@app.route('/artwork/<int: id>')
-def artwork(id) :
-    data = models.Artwork.query.get(id)
-    if data is None :
-        raise Exception
-    return render_template("artwork.html", result=data)
+# @app.route('/artwork/<int: id>')
+# def artwork(id) :
+#     data = models.Artwork.query.get(id)
+#     if data is None :
+#         raise Exception
+#     return render_template("artwork.html", result=data)
 
 @app.route('/styles')
 def styles():
     return app.send_static_file('style.html')
 
-@app.route('/style/<int: id>')
-def style(id) :
-    data = models.Style.query.get(id)
-    if data is None :
-        raise Exception
-    return render_template("style.html", result=data)
+# @app.route('/style/<int: id>')
+# def style(id) :
+#     data = models.Style.query.get(id)
+#     if data is None :
+#         raise Exception
+#     return render_template("style.html", result=data)
 
 @app.route('/collections')
 def collections():
     return app.send_static_file('collections.html')
 
-@app.route('/collections/<int: id>')
-def collection(id) :
-    data = models.Collection.query.get(id)
-    if data is None :
-        raise Exception
-    return render_template("collection.html", result=data)
+# @app.route('/collections/<int: id>')
+# def collection(id) :
+#     data = models.Collection.query.get(id)
+#     if data is None :
+#         raise Exception
+#     return render_template("collection.html", result=data)
 
 @app.route('/temp')
 def temp():
