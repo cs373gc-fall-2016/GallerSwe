a081bf7cd749dfb9a7590225852c16beebbcbc53
Cullen Bounds <cwillbounds@gmail.com>
2016-11-03 17:56:27 -0500

    added artists table

--- app/static/artwork/controller.js
+++ app/static/artwork/controller.js
@@ -2,11 +2,12 @@ angular.module('ArtSnob')
 .controller('artworkController', ['$scope', 'Artwork',
     function($scope, Artwork) {
         'use strict';
-        
+
         $scope.reload = function() {
 			Artwork.get(function(response) {
 				$scope.response = response
-                $scope.objects = response.objects
+            $scope.objects = response.objects
+          console.log("response is ", $scope.response)
 			});
         }
 
