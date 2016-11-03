e50af8cdf19b3caa09df4673d2f6fd4c4f06ac11
courtbohrer <cbohrer@utexas.edu>
2016-11-02 20:13:15 -0500

    api implementation v1

---
+++ app/api.py
@@ -0,0 +1,67 @@
+from flask_restless import APIManager
+from model import Artist, Artwork, Style, Collection
+
+
+def bind_api(app):
+    """ Bind the api to the app """
+    api = APIManager(app, flask_sqlalchemy_db=model.db)
+
+    api.create_api(
+        model.Artist
+        collection_name='artist',
+        url_prefix='/api/',
+        include_columns=[
+            'id',
+            'name',
+            'gender',
+            'birthday',
+            'hometown',
+            'artworks',
+            'thumbnail',
+        ]
+    )
+
+    api.create_api(
+        model.Artwork
+        collection_name='artwork',
+        url_prefix='/api/',
+        include_columns=[
+            'id',
+            'title',
+            'category',
+            'medium',
+            'date',
+            'artists',
+            'styles',
+            'thumbnail',
+        ]
+    )
+
+    api.create_api(
+        model.Style
+        collection_name='style',
+        url_prefix='/api/',
+        include_columns=[
+            'id',
+            'name',
+            'description',
+            'artworks',
+            'number_of_artists',
+            'thumbnail',
+        ]
+    )
+
+    api.create_api(
+        model.Collection
+        collection_name='collection',
+        url_prefix='/api/',
+        include_columns=[
+            'id',
+            'type',
+            'name',
+            'artworks',
+            'website',
+        ]
+    )
+
+    manager.init_app(app)
