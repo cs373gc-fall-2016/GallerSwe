b387e000514cd64d4d267fe5a824bdb743baa648
Cullen Bounds <cwillbounds@gmail.com>
2016-11-04 13:30:46 -0500

    debug

--- app/app.py
+++ app/app.py
@@ -152,10 +152,10 @@ class Style(DB.Model):
     id = DB.Column(DB.String(ID_CHARS), primary_key=True)
     name = DB.Column(DB.String(NAME_CHARS))
     description = DB.Column(DB.String)
-    image = DB.Column(DB.String(LINK_CHARS))
+    image = DB.Column(DB.String(LINK_CHARS)
     artworks = DB.relationship('Artwork', back_populates='styles', secondary=ARTWORK_STYLE)
 
-    def __init__(self, id, name, description, image):
+    def __init__(self, id, name, description, image, nim):
         self.id = id
         self.name = name
         self.description = description
